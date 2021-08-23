9fa4e82caee47a16e875e82de363585e065994f3
ShradhaNbr <sradha.nambiar@gmail.com>
2021-08-23 09:57:59 +0530

    Added : Code to get sum of 2 random dice number

---
+++ Sumof2DiceNumber.js
@@ -0,0 +1,7 @@
+//To get sum of 2 random dice number
+let dice1 = (Math.floor(Math.random() * 6) + 1);
+let dice2 = (Math.floor(Math.random() * 6) + 1);
+let sum = dice1 + dice2;
+console.log("Dice 1 : "+ dice1);
+console.log("Dice 2 : "+ dice2);
+console.log("Sum : "+ sum);
